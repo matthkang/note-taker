a1e7cdc112a81e2d41fae8d56cddb14898cbdf1b
matthew <matthkang@gmail.com>
2023-08-29 00:54:57 -0700

    delete note

--- routes/notes.js
+++ routes/notes.js
@@ -1,32 +1,48 @@
 const notes = require('express').Router();
-const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const { readFromFile, readAndAppend, readAndDelete } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
+const dbData = require('../db/db.json')
 
 // GET Route for retrieving all saved notes
 notes.get('/', (req, res) => {
-  console.info(`${req.method} request received to get all notes`);
-  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+    console.info(`${req.method} request received to get all notes`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 // POST Route for saving a new note
 notes.post('/', (req, res) => {
-  console.info(`${req.method} request received to add a notes`);
-  console.log(req.body);
+    console.info(`${req.method} request received to add a note`);
+    console.log(req.body);
 
-  const { title, text } = req.body;
+    const { title, text } = req.body;
 
-  if (req.body) {
-    const newNote = {
-      title,
-      text,
-      note_id: uuid(),
-    };
+    if (req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuid(),
+        };
 
-    readAndAppend(newNote, './db/db.json');
-    res.json(`Note added successfully 🗒️`);
-  } else {
-    res.error('Error in adding note');
-  }
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully 🗒️`);
+    } else {
+        res.error('Error in adding note');
+    }
+});
+
+notes.delete('/:id', (req, res) => {
+    console.log("delete api");
+    const requestedId = req.params.id;
+
+    // Iterate through the id name to check if it matches `req.params.id`
+    for (let i = 0; i < dbData.length; i++) {
+        if (requestedId === dbData[i].id) {
+            readAndDelete(dbData[i], './db/db.json');
+        }
+    }
+
+    // Return a message if the id doesn't exist in the json
+    return res.json('No match found');
 });
 
 module.exports = notes;
